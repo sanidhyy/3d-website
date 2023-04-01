dd96d620d611115d2ff9ae2320b6585240019970
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-01 18:42:57 +0530

    backend comments added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
+// imports
+
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+// routes
 import dalleRoutes from "./routes/dalle.routes.js";
 
+// initialize environment variables
 dotenv.config();
 
+// port
 const PORT = process.env.PORT || 8080;
 
+// initialize express app
 const app = express();
 
+// add middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+// add routes
 app.use("/api/v1/dalle", dalleRoutes);
 
+// handle get request
 app.get("/", (_, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
 });
 
+// start server
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,25 +1,34 @@
+// imports
 import express from "express";
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
+// initialize environment variables
 dotenv.config();
 
+// initialize router
 const router = express.Router();
 
+// config openai's api key
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// initialize openai instance
 const openai = new OpenAIApi(config);
 
+// handle get route
 router.route("/").get((_, res) => {
   res.status(200).json({ message: "Hello from Dall.E Routes" });
 });
 
+// handle post route
 router.route("/").post(async (req, res) => {
   try {
+    // get prompt
     const { prompt } = req.body;
 
+    // send open ai request
     const response = await openai.createImage({
       prompt,
       n: 1,
@@ -27,10 +36,13 @@ router.route("/").post(async (req, res) => {
       response_format: "b64_json",
     });
 
+    // store image data from response
     const image = response.data.data[0].b64_json;
 
+    // send 200 (ok) response
     res.status(200).json({ photo: image });
   } catch (error) {
+    // send 500 (server error) response
     console.log("Error in fetching image from dalle", error);
     res.status(500).json({ message: "Something went wrong", error });
   }
