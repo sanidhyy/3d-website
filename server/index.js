73908d5424e599f70db76fc43585b2d2b781efe9
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-01 15:40:31 +0530

    dalle routes added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import dalleRoutes from "./routes/dalle.routes.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
