36aad826bcb086c460a5d64d039a7ac290827947
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-01 11:53:21 +0530

    shirt texture created

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+import Shirt from "./Shirt";
+// import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
