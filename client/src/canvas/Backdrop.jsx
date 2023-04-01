8ad19e3b02e8f697e13b97e2b86d324ad2a9c731
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-01 12:35:57 +0530

    major backdrop issues fixed

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -2,9 +2,34 @@ import React, { useRef } from "react";
 import { easing } from "maath";
 import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 
 const Backdrop = () => {
   const shadows = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    // keep track of screen width
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+
+    // make model responsive
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // set shadow camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+  });
 
   return (
     <AccumulativeShadows
@@ -12,11 +37,10 @@ const Backdrop = () => {
       temporal
       frames={60}
       alphaTest={0.85}
-      scale={10}
+      scae={10}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
-      {/* top right light */}
       <RandomizedLight
         amount={4}
         radius={9}
@@ -24,8 +48,6 @@ const Backdrop = () => {
         ambient={0.25}
         position={[5, 5, -10]}
       />
-
-      {/* right light */}
       <RandomizedLight
         amount={4}
         radius={5}
