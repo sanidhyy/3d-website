a58d7e22576feba8a875727df094b2d60106be58
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-04-01 17:01:53 +0530

    mongoose package removed

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
 });
 
