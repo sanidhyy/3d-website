3543868f4db8bae2b42ee43ee7572f8d8fa0f204
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-02-12 14:15:01 +0530

    fix: blank screen on app load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Canvas from "./canvas";
 import Home from "./pages/Home";
 import Customizer from "./pages/Customizer";
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <main className="app transition-all ease-in">
+          <Home />
+          <Canvas />
+          <Customizer />
+        </main>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 // App
 const App = () => {
   return (
-    <main className="app transition-all ease-in">
-      <Home />
-      <Canvas />
-      <Customizer />
-    </main>
+    <ErrorBoundary>
+      <main className="app transition-all ease-in">
+        <Home />
+        <Canvas />
+        <Customizer />
+      </main>
+    </ErrorBoundary>
   );
 };
 
